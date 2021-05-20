a4f03065ffc5b3923b6f03f01d99b3c72b5fde53
rameydj <rameydj@rose-hulman.edu>
2021-05-20 06:35:09 -0400

    Finished for Demo happy 6:35 am

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,15 @@ public class Main {
 		Load.populateData("StateParkList");
 		HashMap<String, MapNode> map = Load.createLocations();
 		Load.autoGenNeighbours(1.0, map);
-//		for(MapNode mp : map.values()) {
-//			System.out.println(mp.toString() + " with neighbors: " + mp.getNeighbors().toString());
-//		}
-		AStarSearch search = new AStarSearch(map);
-		MapNode st = map.get("Indiana Dunes");
-		MapNode go = map.get("Turkey Run");
-		LinkedList<MapNode> result = search.search(st, go);
-		System.out.println(result);
-		System.out.println(search.getFinalDist(result));
+		for(MapNode mp : map.values()) {
+			System.out.println(mp.toString() + " with neighbors: " + mp.getNeighbors().toString());
+		}
+//		AStarSearch search = new AStarSearch(map);
+//		MapNode st = map.get("Indiana Dunes");
+//		MapNode go = map.get("Turkey Run");
+//		LinkedList<MapNode> result = search.search(st, go);
+//		System.out.println(result);
+//		System.out.println(search.getFinalDist(result));
 //		System.out.println("X: " + map.get("Indiana Dunes").getx() + "Y: " + map.get("Indiana Dunes").gety());
 //		System.out.println("X: " + map.get("Turkey Run").getx() + "Y: " + map.get("Turkey Run").gety());
 //		System.out.println("X: " + map.get("Shades").getx() + "Y: " + map.get("Shades").gety());
